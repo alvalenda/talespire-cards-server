487625b10399cdc46064a62528f71c6fac3177a0
Flavio Alvarenga <flavio.alva@outlook.com>
2023-02-13 11:27:31 -0300

    chore(app): basic bootstrap setup

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
+import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const logger = new Logger('bootstrap');
+
+  app.enableCors();
+
+  await app.listen(3000).then(() => {
+    logger.log('Server is running on port 3000');
+  });
 }
 bootstrap();
