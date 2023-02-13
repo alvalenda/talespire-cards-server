4e9e0f6b5903878cdfbcd1affa823d322afae5e3
Flavio Alvarenga <flavio.alva@outlook.com>
2023-02-13 11:39:58 -0300

    feat(app): implement common config-constants

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,15 @@
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { server } from './common/config/config-constants';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const logger = new Logger('bootstrap');
+  const port = server.port;
 
-  app.enableCors();
-
-  await app.listen(3000).then(() => {
-    logger.log('Server is running on port 3000');
+  await app.listen(port).then(() => {
+    logger.log(`Server is running on port ${port}`);
   });
 }
 bootstrap();
